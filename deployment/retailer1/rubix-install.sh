eb88c7bda4f8ef3a3afae9a19f6ce141230c51c7
nitishkumar1288 <142576370+nitishkumar1288@users.noreply.github.com>
2023-08-21 18:09:18 +0530

    Update rubix-install.sh

--- deployment/retailer1/rubix-install.sh
+++ deployment/retailer1/rubix-install.sh
@@ -4,20 +4,4 @@ echo ":::::::::::::: START SCRIPT :::::::::::::::::"
 #cd /opt/atlassian/pipelines/agent/build/
 echo "Finding all files under directory first........"
 
-
-function require_environment_variable() {
- required_var_name="$1";
- required_var_value=${!required_var_name}
- if [[ $required_var_value ]];
- then
- echo "Success - Found $required_var_name in environment variables..."
- else
- abort_on_failure "Failure - $required_var_name environment variable not
-set. Please set this environment variable and try again."
- fi;
-}
-
-accountId="DEV_ACCOUNT"
-require_environment_variable ${accountId}
-
-echo "checkaccountId=${!accountId}"
+echo "ENVIRONMENT_VARIABLE_NAME" ${ENVIRONMENT_VARIABLE_NAME}
