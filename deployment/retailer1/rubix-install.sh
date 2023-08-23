b86b75f9dbcd2145838ab9c5e8e5249ddee08ecc
garglokesh12 <44775208+garglokesh12@users.noreply.github.com>
2023-08-23 09:20:10 +0400

    Update rubix-install.sh

--- deployment/retailer1/rubix-install.sh
+++ deployment/retailer1/rubix-install.sh
@@ -4,4 +4,4 @@ echo ":::::::::::::: START SCRIPT :::::::::::::::::"
 #cd /opt/atlassian/pipelines/agent/build/
 echo "Finding all files under directory first........"
 
-echo "ENVIRONMENT_VARIABLE_NAME" ${ENVIRONMENT_VARIABLE_NAME}
+echo "ENVIRONMENT_VARIABLE_NAME" ${DEV_ACCOUNT}
